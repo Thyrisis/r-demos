c0cfde34e9e0da63300443cad68c98e06d61e83e
Richard Parfitt <richard.parfitt@freedom-finance.com>
2024-07-02 16:38:01 +0100

    added clean-install.R for ease

---
+++ clean-install.R
@@ -0,0 +1,7 @@
+options(repos ='https://cran.r-project.org/'
+, unzip = 'internal'
+, download.file.method ='libcurl'
+, Ncpus = parallel::detectCores())
+install.packages('requiRements')
+requiRements::install(c('tidyverse', 'data.table', 'gt', 'keyring', 'odbc', 'paws', 'reticulate', 'sf', 'slider', 'webshot2'
+, 'tidyquant', 'geomtextpath', 'bbc/bbplot@master'))
